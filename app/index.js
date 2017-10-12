62205f30b86d39c731f9326132fc45754139264a
Enrico Vogt <info@enricovogt.de>
2017-10-12 13:23:19 +0200

    bs4

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 require('file-loader?name=[name].[ext]!../index.html');
 
-import '../app/main.scss';
 import 'jquery/dist/jquery.min.js'
-import 'bootstrap-sass/assets/javascripts/bootstrap.min.js'
+import 'bootstrap'
+import '../app/main.scss';
 
